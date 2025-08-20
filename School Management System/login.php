41f93c10783d2bf060cd78ec78b6bc49a40a3600
mdsameerahmed01 <md.sameerahmed01@gmail.com>
2025-08-20 21:55:53 +0530

    Add files via upload

---
+++ School Management System/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login-SYMGA School</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.png">
+</head>
+
+<body class="body-home">
+    <div class="black-fill"><br>
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <br /><br /><br />
+            <form class="login">
+                <h3>Login</h3>
+                <div class="mb-3">
+                    <label class="form-label">Username</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Login As</label>
+                    <select class="form-control">
+                        <option value="1">Admin</option>
+                        <option value="1">Teacher</option>
+                        <option value="1">Student</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary"><a class="nav-link" href="index.php">Login</a></button>
+            </form>
+            <br /><br /><br /><br /><br />
+            <div class="text-center text-light mt-5">
+                Copyright &copy; 2025 SYMGA School. All rights reserved.
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
